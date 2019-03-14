6357efa9e4365f022f12babc1984941a466680db
Fingann <Fingann92@gmail.com>
2019-03-14 08:39:13 +0100

    Update NoSpotsLeft.cs

--- IfiNavetNotifier/BusinessRules/Rules/NoSpotsLeft.cs
+++ IfiNavetNotifier/BusinessRules/Rules/NoSpotsLeft.cs
@@ -1,8 +1,10 @@
+using IfiNavetNotifier.Extentions;
+
 namespace IfiNavetNotifier.BusinessRules.Rules
 {
     public class NoSpotsLeft : IBusinessRule
     {
-        public string RuleName { get; } = "No spots left";
+        public string RuleName { get; } = nameof(AwailableSpots).TitleCaseToRegular();
         public int Priority { get; } = 1;
 
         public bool CheckComplience(IfiEvent database, IfiEvent target)
